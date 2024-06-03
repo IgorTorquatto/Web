42d1caf64559c9685e59935bd8caab83ec5fa88a
Igor <igor.torquato.20@gmail.com>
2024-06-03 18:08:32 -0300

    add: components

---
+++ React/projeto-cadastro-usuarios/src/components/Title/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const H1 = styled.h1`
+  font-weight: bold;
+  font-size: 34px;
+  line-height: 40px;
+  color: #ffffff;
+  margin-bottom: 80px;
+  text-align: center;
+`;
